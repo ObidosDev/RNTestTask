dd9a420b5289dd7e6a88b638bde85e67fffc5f41
Vitalii Obideiko <vitaliy.obideyko@gmail.com>
2022-12-07 10:16:16 +0300

    Clear

--- src/AppWithNavigationState.js
+++
@@ -1,9 +0,0 @@
-
-import { connect } from 'react-redux'
-import App from './App'
-
-const mapStateToProps = state => ({
-  nav: state.nav
-})
-
-export default connect(mapStateToProps)(App)
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { AppRegistry } from 'react-native'
-import Application from './AppWithStoreProvider'
-
-export default function run() {
-  AppRegistry.registerComponent('RNTestTask', () => () => {
-    return <Application />
-  })
-}
